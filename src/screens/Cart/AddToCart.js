79f7885f46903418b16504859894001ce47cc89e
elisedoe <developersweb002@gmail.com>
2023-08-31 14:11:31 +0530

    add header in cart section

--- src/screens/Cart/AddToCart.js
+++ src/screens/Cart/AddToCart.js
@@ -1,8 +1,38 @@
-import { View , Text} from "react-native";
+import { View, Text , Image} from "react-native";
+import { useLayoutEffect } from "react";
+import MenuBackArrow from '../../components/menubackarrow/menubackarrow';
+import cartIcone from '../../../assets/sidebaricons/cart.png';
+const AddToCart = (props) => {
+    const { navigation } = props;
 
-const AddToCart = ()=>{
-
-    return(
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <MenuBackArrow
+                    onPress={() => {
+                        navigation.navigate('Home');
+                    }}
+                />
+            ),
+            headerTitle: () => (
+                <View style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    width: "100%"
+                }}>
+                    <Text style={{
+                        textAlign: "center",
+                        fontSize: 25, fontWeight: 700, width: "100%"
+                    }}>Cart</Text>
+                </View>
+            ),
+            headerRight: () => 
+            <View >
+                <Image source={cartIcone}></Image>
+            </View>,
+        });
+    }, []);
+    return (
         <View>
             <Text>AddToCart</Text>
         </View>
