a5aba751db632c6b4323a5a971f81c64f01566cd
developer03 <developersweb002@gmail.com>
2023-09-01 15:02:44 +0530

    Added loader in the article details Page

--- src/screens/Artical-details/styles.js
+++ src/screens/Artical-details/styles.js
@@ -392,6 +392,11 @@ const styles = StyleSheet.create({
         borderRadius:10,
         paddingHorizontal:20,
         paddingVertical:10
+    },
+    loader: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
     }
 });
 
