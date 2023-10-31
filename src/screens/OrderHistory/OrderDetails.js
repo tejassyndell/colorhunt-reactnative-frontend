0797809dd771287b457317d6526b2121bd900ead
elisedoe <developersweb002@gmail.com>
2023-10-31 14:20:20 +0530

    Remove the order history message from orders screen

--- src/screens/OrderHistory/OrderDetails.js
+++ src/screens/OrderHistory/OrderDetails.js
@@ -421,9 +421,6 @@ const OrderDetails = (props) => {
     <body>
     <table border="1" cellspacing="0" cellpadding="10" width="100%">
         <tr>
-            <td colspan="12" style="text-align: center; font-weight: bold">SALES ORDER</td>
-        </tr>
-        <tr>
             <td colspan="12" style="text-align: center; background-color: black; color: white;font-weight: bold">
                 COLORHUNT
             </td>
